8b80080b2e135b211457c69d8967a9fafdf9a099
Russell Chisholm <rchisholm@marian.org>
2020-09-01 14:33:55 -0800

    added actionbar icons

--- src/app/page/message/introduction/introduction.component.ts
+++ src/app/page/message/introduction/introduction.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
+import { RouterExtensions } from "nativescript-angular/router";
 import { DataService, TextItem } from "~/app/shared/data.service";
 
 @Component({
@@ -9,9 +9,16 @@ import { DataService, TextItem } from "~/app/shared/data.service";
 export class IntroductionComponent implements OnInit {
     items: Array<TextItem>;
 
-    constructor(private _itemService: DataService) { }
+    constructor(
+        private _itemService: DataService,
+        private _routerExtensions: RouterExtensions
+    ) { }
 
     ngOnInit(): void {
         this.items = this._itemService.getTextItems("introduction");
+    }
+
+    onBackTap(): void {
+        this._routerExtensions.back();
     }
 }
