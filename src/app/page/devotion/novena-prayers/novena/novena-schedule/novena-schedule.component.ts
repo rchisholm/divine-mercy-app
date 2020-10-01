f1aad65cabb3a83dcdce28d343cb809f27dfc1fd
Russell Chisholm <rchisholm@marian.org>
2020-10-01 15:25:36 -0800

    front-end of schedule notifications

--- src/app/page/devotion/novena-prayers/novena/novena-schedule/novena-schedule.component.ts
+++ src/app/page/devotion/novena-prayers/novena/novena-schedule/novena-schedule.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { DatePicker } from "tns-core-modules/ui/date-picker";
 import { TimePicker } from "tns-core-modules/ui/time-picker";
-import { SegmentedBar, SegmentedBarItem, SelectedIndexChangedEventData } from "tns-core-modules/ui/segmented-bar";
 
 @Component({
     selector: "NovenaSchedule",
@@ -17,24 +16,11 @@ export class NovenaScheduleComponent implements OnInit {
     minute: number;
     second: number;
 
-    controlOptions: Array<SegmentedBarItem> = [];
-    startNovenaOptions: SegmentedBarItem;
-    clearNotificationsItem: SegmentedBarItem;
-    controlIndex: number;
-
     constructor(
         private router: RouterExtensions
     ) { }
 
     ngOnInit(): void {
-
-        this.startNovenaOptions = new SegmentedBarItem();
-        this.startNovenaOptions.title = "Start Novena Notifications";
-        this.clearNotificationsItem = new SegmentedBarItem();
-        this.clearNotificationsItem.title = "Clear Notifications";
-
-        this.controlOptions.push(this.startNovenaOptions, this.clearNotificationsItem);
-        this.controlIndex = null;
 
         this.scheduleDate = new Date();
         this.year = this.scheduleDate.getFullYear();
@@ -82,12 +68,13 @@ export class NovenaScheduleComponent implements OnInit {
         this.scheduleDate = new Date(this.year, this.month , this.day, this.hour, this.minute);
     }
 
-    onSelectedIndexChange(args: SelectedIndexChangedEventData) {
-        const segmentedBar = args.object as SegmentedBar;
-        const oldIndex = args.oldIndex;
-        const newIndex = args.newIndex;
-        console.log(newIndex);
-        this.controlIndex = null;
+    scheduleNotifications() {
+        console.log("scheduling notifications starting at date:");
+        console.log(this.scheduleDate);
+    }
+
+    clearNotifications() {
+        console.log("clearing notifications...");
     }
 
     onBackTap(): void {
