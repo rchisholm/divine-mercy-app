8e45437e0f180e05c54c9127a1b6610d24c7fe26
Russell Chisholm <rchisholm@marian.org>
2020-11-09 16:34:43 -0900

    all resource items

--- src/app/page/message/resources/resources.component.ts
+++ src/app/page/message/resources/resources.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService, ResourceItem } from "~/app/shared/data.service";
 import { RouterExtensions } from "nativescript-angular";
+import { openUrl } from "tns-core-modules/utils/utils";
+import { dial } from "nativescript-phone";
 
 @Component({
     selector: "Resources",
@@ -20,6 +22,16 @@ export class ResourcesComponent implements OnInit {
 
     onBackTap(): void {
         this.router.back();
+    }
+
+    openLink(item): void {
+        if (item.tel) {
+            dial(item.tel, true);
+        } else if (item.link) {
+            openUrl(item.link);
+        } else {
+            console.error("no link or tel for resouce item id #" + item.id);
+        }
     }
 
 }
