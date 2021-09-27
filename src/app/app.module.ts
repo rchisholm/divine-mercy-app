1bba09a138ac8544970692aa96a5f2f2e366313a
Russell Chisholm <rchisholm@marian.org>
2021-09-27 13:11:32 -0700

    added accessibility module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptAnimationsModule } from "nativescript-angular/animations";
 import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { NotaAccessibilityExtModule } from '@nota/nativescript-accessibility-ext/angular';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -14,7 +15,8 @@ import { AppComponent } from "./app.component";
         NativeScriptModule,
         AppRoutingModule,
         NativeScriptAnimationsModule,
-        NativeScriptHttpClientModule
+        NativeScriptHttpClientModule,
+        NotaAccessibilityExtModule
     ],
     declarations: [
         AppComponent
